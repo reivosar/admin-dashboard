c853a4b212d5b5b2344747707c93e6b608c3f6ee
reivosar <{ID}+{username}@users.noreply.github.com>
2024-03-24 03:33:09 +0900

    redesigned sort icon

--- src/components/utils/SortIcon.tsx
+++ src/components/utils/SortIcon.tsx
@@ -1,11 +1,12 @@
+import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/solid";
 import React from "react";
 
 export const SortIcon: React.FC<{ direction: string }> = ({ direction }) => (
-  <span
-    className={`inline-block ml-2 ${
-      direction === "asc" ? "text-blue-600" : "text-red-600"
-    }`}
-  >
-    {direction === "asc" ? "▲" : "▼"}
-  </span>
+  <>
+    {direction === "asc" ? (
+      <ChevronUpIcon className="inline-block ml-2 w-5 h-5" />
+    ) : (
+      <ChevronDownIcon className="inline-block ml-2 w-5 h-5" />
+    )}
+  </>
 );
